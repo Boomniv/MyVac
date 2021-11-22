8def9bb86a8045b21f3a8546d178838f9fb57fd1
Boomniv <nivburlak@gmail.com>
2021-11-22 15:08:16 +0200

    Updated database to include children table

--- app/src/main/java/com/example/myvac/DBHelper.java
+++ app/src/main/java/com/example/myvac/DBHelper.java
@@ -18,7 +18,17 @@ public class DBHelper extends SQLiteOpenHelper {
     public  static final String EMAIL = "email";
 
 
+    public  static final String TABLE_NAME1 = "children";
+    public  static final String PARENT_ID = "id";
+    public  static final String DATE_OF_BIRTH = "dateOfBirth";
+    public  static final String FULL_NAME = "fullName";
+    public  static final String WEIGHT = "weight";
+    public  static final String HEIGHT = "height";
+    public  static final String HEAD_SIZE = "headSize";
+    public  static final String DOCTOR_NAME= "doctorName";
+
     public String SQL_Create="",SQL_Delete="";
+    public String SQL_Create1="",SQL_Delete1="";
 
     public DBHelper(@Nullable Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -35,12 +45,27 @@ public class DBHelper extends SQLiteOpenHelper {
         SQL_Create+=EMAIL+" TEXT);";
 
         sqLiteDatabase.execSQL(SQL_Create);
+
+        SQL_Create1="CREATE TABLE " +TABLE_NAME1+" (";
+        SQL_Create1+=PARENT_ID+" TEXT, ";
+        SQL_Create1+=DATE_OF_BIRTH+" TEXT, ";
+        SQL_Create1+=FULL_NAME+" TEXT, ";
+        SQL_Create1+=WEIGHT+" TEXT, ";
+        SQL_Create1+=HEIGHT+" TEXT, ";
+        SQL_Create1+=HEAD_SIZE+" TEXT, ";
+        SQL_Create1+=DOCTOR_NAME+" TEXT);";
+
+        sqLiteDatabase.execSQL(SQL_Create1);
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
         SQL_Delete="DROP TABLE IF EXISTS "+TABLE_NAME;
         sqLiteDatabase.execSQL(SQL_Delete);
+
+        SQL_Delete1="DROP TABLE IF EXISTS "+TABLE_NAME1;
+        sqLiteDatabase.execSQL(SQL_Delete1);
 
         onCreate(sqLiteDatabase);
     }
