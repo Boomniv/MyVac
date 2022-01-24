89c952c576b201debc31adedf98902c4e88dd8bd
niv <nivburlak@gmail.com>
2022-01-24 15:45:35 +0200

    added credits

---
+++ app/src/main/java/com/example/myvac/Credits.java
@@ -0,0 +1,15 @@
+package com.example.myvac;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Credits extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credits);
+    }
+}
